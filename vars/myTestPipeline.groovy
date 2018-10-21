2fc5cfcb1add0f60b69a81f2a337f2c8afab34ff
Marvin Andia <mandia1204@gmail.com>
2018-10-21 13:56:37 -0500

    testing ssh12

--- vars/myTestPipeline.groovy
+++ vars/myTestPipeline.groovy
@@ -26,7 +26,9 @@ def call(Map pipelineParams) {
             stage('deploy') {
                 steps {
                     copyFiles()
-                    sshUtil.publish configName: 'kube-server', command:'./test.sh', removePrefix: 'dist', sourceFiles:'dist/**', dir: 'copied'
+                    script {
+                        sshUtil.publish configName: 'kube-server', command:'./test.sh', removePrefix: 'dist', sourceFiles:'dist/**', dir: 'copied'
+                    }
                 }
             }
             stage('Test') {
